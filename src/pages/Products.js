ac87f83bc83bf4a693b49cf8f2caf9c1bc969b97
Rahman1234562 <126483990+Rahman1234562@users.noreply.github.com>
2023-09-20 12:04:08 -0700

    updated files

---
+++ src/pages/Products.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import product1 from "../asset/products/1.png";
+import product2 from "../asset/products/2.png";
+import product3 from "../asset/products/3.png";
+import product4 from "../asset/products/4.png";
+import product5 from "../asset/products/5.png";
+
+
+
+export const Products = [
+    {
+        id : 1,
+        productName: "Iphone",
+        price : 199.0,
+        productImage: product1,
+    },
+    {
+        id : 2,
+        productName: "MacBook pro(2023)",
+        price : 399.0,
+        productImage: product2,
+    },
+    {
+        id : 3,
+        productName: "cannon M50 camera",
+        price : 99.0,
+        productImage: product3,
+    },
+    {
+        id : 4,
+        productName: "WLS wen jacket",
+        price : 49.0,
+        productImage: product4,
+    },
+    {
+        id : 5,
+        productName: "LED light strip",
+        price : 129.0,
+        productImage: product5,
+    },
+]
