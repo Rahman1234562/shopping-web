25184b3f6ffb552ddfb7ea87eb3f15476b8aeb3d
Rahman1234562 <126483990+Rahman1234562@users.noreply.github.com>
2023-09-22 11:58:10 -0700

    updated files

--- src/Products.js
+++ src/Products.js
@@ -4,6 +4,11 @@ import product2 from "./asset/2.png";
 import product3 from "./asset/3.png";
 import product4 from "./asset/4.png";
 import product5 from "./asset/5.png";
+import product6 from "./asset/6.jpeg";
+import product7 from "./asset/7.webp";
+import product8 from "./asset/8.webp";
+import product9 from "./asset/9.jpg";
+import product10 from "./asset/10.jpg";
 
 
 
@@ -38,4 +43,38 @@ export const PRODUCTS = [
         price : 129.0,
         productImage: product5,
     },
+    {
+        id : 6,
+        productName: "acer",
+        price : 150.09,
+        productImage: product6,
+    },
+    {
+        id : 7,
+        productName: "XDCAM",
+        price : 99.9,
+        productImage: product7,
+    },
+
+    {
+        id : 8,
+        productName: "men shirt",
+        price : 49.9,
+        productImage: product8,
+    },
+
+    {
+        id : 9,
+        productName: "Men shirt",
+        price : 50.00,
+        productImage: product9,
+    },
+
+    {
+        id : 10,
+        productName: "iphone 15 pro",
+        price : 299.9,
+        productImage: product10,
+    },
+    
 ]
--- src/compnents/Navbar.jsx
+++ src/compnents/Navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
         <div className="navbar">
         <div className="link">
             <Link to="/">Shop</Link>
-            <Link to="/cart"><ShoppingCart size={32}/></Link>
+            <Link to="/cart"><ShoppingCart size={28} className='user-cart'/></Link>
         </div>
         </div>
   )
